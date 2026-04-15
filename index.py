18c03afd2caefbdb9d0d6ef3b4009842aa4fcbd8
dkozlov-collab <d.kozlov@life-pay.ru>
2026-04-15 11:47:01 +0300

    Update index.py

--- index.py
+++ index.py
@@ -99,10 +99,3 @@ URL = "https://docs.google.com/spreadsheets/d/1Q4MGhp0KsLb57Ouqu58j_Md5zoFgAhFd3
 # Читаем данные со 150 строки (пропускаем 149)
 df_log = pd.read_csv(URL, skiprows=149).fillna("")
 
-st.title("🚚 Мониторинг отгрузок (со 150 стр.)")
-search = st.text_input("🔍 Найти посылку по треку, городу или дате")
-
-if search:
-    df_log = df_log[df_log.apply(lambda r: r.astype(str).str.contains(search, case=False).any(), axis=1)]
-
-st.dataframe(df_log, use_container_width=True)
