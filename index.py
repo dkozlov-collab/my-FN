837691927861ee99f776b8417429e4b2aa1e462c
dkozlov-collab <d.kozlov@life-pay.ru>
2026-04-15 13:11:50 +0300

    Update index.py

--- index.py
+++ index.py
@@ -69,7 +69,7 @@ if not df_raw.empty:
     if sel_city: df = df[df[c_city].astype(str).isin(sel_city)]
 
     # --- ТАБЛИЦА ---
-    st.write("### 📋 Реестр (A:R)")
+    st.write("### 📋 Остатки
     st.dataframe(df, use_container_width=True, hide_index=True)
 
     # --- НИЖНИЕ ИТОГИ (ЦИФРЫ) ---
