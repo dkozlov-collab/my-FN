b66fc3e62d1d26ee3007005ada4ad48cd11e0606
dkozlov-collab <d.kozlov@life-pay.ru>
2026-04-16 17:45:58 +0300

    Update index.py

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import pandas as pd
 from auth_logic import login_system
     
     # --- 1. ПРОВЕРКА АВТОРИЗАЦИИ (ЗАМОК) ---
-    is_auth, user_login, user_filter = login_system()
+is_auth, user_login, user_filter = login_system()
     
     if is_auth:
         # Настройка страницы
