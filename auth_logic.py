d873a105f67c58374aae3f0a86878a1c94c7ccf4
dkozlov-collab <d.kozlov@life-pay.ru>
2026-04-16 16:03:41 +0300

    Update auth_logic.py

--- auth_logic.py
+++ auth_logic.py
@@ -1,40 +1,35 @@
-import streamlit as st
+mport streamlit as st
 
 def login_system():
-    # 1. Список ПИН-кодов и доступов
-    # Формат: "ПИН-КОД": ("Имя пользователя", "Фильтр организации")
+    # ПИН-код: ("Название для интерфейса", "Ключевое слово для поиска в таблице")
     USERS = {
-        "1121018100": ("ООО АТМ АЛЬЯНС СОЛЮШИНС", "ООО АТМ АЛЬЯНС СОЛЮШИНС"),  # Только эта орг.
-        "7734595315": ("Менеджер 2", "ООО АРЕС-КОМПАНИ-М"),      # Только эта орг.
-        "5321203280": ("Менеджер 3", "ООО БР"),                  # Только эта орг.
-        "9718146933": ("Менеджер 4", "Автоматизация Бизнеса"),    # Только эта орг.
-        "9701010050": ("Администратор", "Все")                   # Видит ВСЕ организации
+        "1111": ("АТМ АЛЬЯНС", "АТМ"),
+        "2222": ("АРЕС-КОМПАНИ", "АРЕС"),
+        "3333": ("ООО БР", "БР"),
+        "4444": ("АВТОМАТИЗАЦИЯ", "Автомат"),
+        "0000": ("АДМИНИСТРАТОР", "Все")
     }
 
-    # Инициализация состояния сессии
     if 'auth' not in st.session_state:
         st.session_state.auth = False
         st.session_state.user = ""
         st.session_state.filter = "Все"
 
-    # Если уже авторизован
     if st.session_state.auth:
         return True, st.session_state.user, st.session_state.filter
 
-    # Окно входа (Интерьер как на LIFE PAY)
-    st.markdown("<h2 style='text-align: center; color: #0052FF;'>LIFE PAY | Вход</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center; color: #0052FF;'>LIFE PAY | ERP</h2>", unsafe_allow_html=True)
     
-    with st.container():
-        col1, col2, col3 = st.columns([1, 1, 1])
-        with col2:
-            pin = st.text_input("Введите ПИН-код", type="password", help="4 цифры")
-            if st.button("Войти", use_container_width=True):
-                if pin in USERS:
-                    st.session_state.auth = True
-                    st.session_state.user = USERS[pin][0]
-                    st.session_state.filter = USERS[pin][1]
-                    st.rerun()
-                else:
-                    st.error("Неверный ПИН-код")
+    col1, col2, col3 = st.columns([1, 1, 1])
+    with col2:
+        pin = st.text_input("Введите ПИН-код", type="password")
+        if st.button("ВОЙТИ", use_container_width=True):
+            if pin in USERS:
+                st.session_state.auth = True
+                st.session_state.user = USERS[pin][0]
+                st.session_state.filter = USERS[pin][1]
+                st.rerun()
+            else:
+                st.error("Неверный ПИН-код")
     
     return False, "", ""
