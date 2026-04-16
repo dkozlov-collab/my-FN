9783a355864939860808412fd5c63066f9bfed44
dkozlov-collab <d.kozlov@life-pay.ru>
2026-04-16 16:20:53 +0300

    Update index.py

--- index.py
+++ index.py
@@ -105,8 +105,11 @@ if is_auth:
             date_val = str(row.iloc[12])  
             org_val  = str(row.iloc[2])   
             city_val = str(row.iloc[1])   
-            ttn_val  = str(row.iloc[13])  # СТОЛБЕЦ N (ТРЕК-НОМЕР)
-            content  = str(row.iloc[7])   
+           ttn_val = str(row.iloc[13])  # ТУТ ИСПРАВИЛ НА iloc
+        raw_content = str(row.iloc[7])
+        content = raw_content.split(',')[0].strip() # ТУТ ДОБАВИЛ ОЧИСТКУ
+        move_val = str(row.iloc[14])
+        raw_content.split(',')[0].strip()
             move_val = str(row.iloc[14])  
 
             # Иконка в заголовке
