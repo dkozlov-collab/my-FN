8f956f9e0ae1b73dafd762e728a95fe93e627f60
dkozlov-collab <d.kozlov@life-pay.ru>
2026-04-17 11:45:35 +0300

    Update index.py

--- index.py
+++ index.py
@@ -58,7 +58,7 @@ if is_auth:
         df_raw = df_raw[mask]
 
     # --- 5. БОКОВАЯ ПАНЕЛЬ ---
-      with st.sidebar:
+ with st.sidebar:
     # Строки 62-64 (у них уже есть отступ, они СЛЕВА):
       st.markdown(...)
       st.write(...)
