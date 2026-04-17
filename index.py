4c4ee00f311b9256c988b5f3500aa1e562e06874
dkozlov-collab <d.kozlov@life-pay.ru>
2026-04-17 10:14:56 +0300

    Update index.py

--- index.py
+++ index.py
@@ -107,7 +107,7 @@ if is_auth:
                 with col2:
                     st.markdown("<span class='info-label'>📄 Номер перемещения (O):</span>", unsafe_allow_html=True)
                     st.markdown(f"<div class='move-number'>{move_val}</div>", unsafe_allow_html=True)
-                    with col2:
+                with col2:
                     st.markdown("<span class='info-label'>📋 Номер перемещения (O):</span>", unsafe_allow_html=True)
                     st.markdown(f"<div class='move-number'>{move_val}</div>", unsafe_allow_html=True)
             
