0e5dd695ecb448ec799f1360e9304e484b256519
dkozlov-collab <d.kozlov@life-pay.ru>
2026-04-17 10:05:49 +0300

    Update auth_logic.py

--- auth_logic.py
+++ auth_logic.py
@@ -1,1 +1,35 @@
+import streamlit as st
 
+def login_system():
+    # ПИН-код: ("Название для интерфейса", "Ключевое слово для поиска в таблице")
+    USERS = {
+        "1111": ("АТМ АЛЬЯНС", "АТМ"),
+        "2222": ("АРЕС-КОМПАНИ", "АРЕС"),
+        "3333": ("ООО БР", "БР"),
+        "4444": ("АВТОМАТИЗАЦИЯ", "Автомат"),
+        "0000": ("АДМИНИСТРАТОР", "Все")
+    }
+
+    if 'auth' not in st.session_state:
+        st.session_state.auth = False
+        st.session_state.user = ""
+        st.session_state.filter = "Все"
+
+    if st.session_state.auth:
+        return True, st.session_state.user, st.session_state.filter
+
+    st.markdown("<h2 style='text-align: center; color: #0052FF;'>LIFE PAY | ERP</h2>", unsafe_allow_html=True)
+    
+    col1, col2, col3 = st.columns([1, 1, 1])
+    with col2:
+        pin = st.text_input("Введите ПИН-код", type="password")
+        if st.button("ВОЙТИ", use_container_width=True):
+            if pin in USERS:
+                st.session_state.auth = True
+                st.session_state.user = USERS[pin][0]
+                st.session_state.filter = USERS[pin][1]
+                st.rerun()
+            else:
+                st.error("Неверный ПИН-код")
+    
+    return False, "", ""
